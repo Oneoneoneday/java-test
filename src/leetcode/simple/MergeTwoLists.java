85c5c964446bfb19a1bdccf35bd7cfc67717bc3a
李永华 <yonghua.li01@hand-china.com>
2019-10-11 17:04:42 +0800

    [FIX] 修改leetcode-21

--- src/leetcode/simple/MergeTwoLists.java
+++ src/leetcode/simple/MergeTwoLists.java
@@ -30,9 +30,6 @@ public class MergeTwoLists {
     }
 
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) {
-            return null;
-        }
         if (l1 == null || l2 == null) {
             return l1 == null ? l2 : l1;
         }
@@ -70,10 +67,11 @@ public class MergeTwoLists {
         if (l1 == null) {
             return l2;
         }
-        else if (l2 == null) {
+        if (l2 == null) {
             return l1;
         }
-        else if (l1.val < l2.val) {
+
+        if (l1.val < l2.val) {
             l1.next = mergeTwoLists1(l1.next, l2);
             return l1;
         }
