a159f4f671d369f464e0296aa40811617b5a0c66
李永华 <yonghua.li01@hand-china.com>
2019-10-11 09:34:28 +0800

    [FIX] 修改 leetcode-20 有效的括号

--- src/leetcode/simple/IsValid.java
+++ src/leetcode/simple/IsValid.java
@@ -1,12 +1,14 @@
 package leetcode.simple;
 
+import java.util.*;
+
 /**
  * leetcode-20 有效的括号
  * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
  * 有效字符串需满足：
  * 左括号必须用相同类型的右括号闭合。
  * 左括号必须以正确的顺序闭合。
- *
+ * <p>
  * 注意空字符串可被认为是有效字符串。
  *
  * @author yonghua.li01@hand-china.com 2019/09/06 16:27
@@ -15,44 +17,38 @@ public class IsValid {
 
     public static void main(String[] args) {
         //System.out.println("(())".indexOf(')'));
-        System.out.println(isValid("()"));
-        System.out.println(isValid("()[]{}"));
+//        System.out.println(isValid("()"));
         System.out.println(isValid("(]"));
-        System.out.println(isValid("([)]"));
-        System.out.println(isValid("{[]}"));
+//        System.out.println(isValid("()[]{}"));
+//        System.out.println(isValid("([)]"));
+//        System.out.println(isValid("{[]}"));
     }
 
     public static boolean isValid(String s) {
-        //判断空字符串和字符串长度不为偶数，或者为偶数但是左右括号数量不匹配
         if ("".equals(s)) {
             return true;
-        } else if (null == s || s.length() % 2 != 0) {
-            return false;
         }
-        //记录左右括号出现的数量
-        //小括号( ) 中括号[] 大括号{}
-        int left1 = 0, right1 = 0, left2 = 0, right2 = 0, left3 = 0, right3 = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                left1++;
-            } else if (s.charAt(i) == ')') {
-                right1++;
-            } else if (s.charAt(i) == '[') {
-                left2++;
-            } else if (s.charAt(i) == ']') {
-                right2++;
-            } else if (s.charAt(i) == '{') {
-                left3++;
-            } else if (s.charAt(i) == '}') {
-                right3++;
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>(3);
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        stack.push(s.charAt(0));
+        //栈原理
+        for (int i = 1; i < s.length(); i++) {
+            //如果栈顶是右括号，则始终匹配不上括号
+            if (!stack.isEmpty() && !map.containsKey(stack.peek())) {
+                return false;
             }
+            //取栈顶对应的有括号与当前要推入的对比，如果能匹配上括号则推出栈顶
+            boolean flag = !stack.isEmpty() && map.get(stack.peek()) == s.charAt(i);
+            if (flag) {
+                stack.pop();
+                continue;
+            }
+            stack.push(s.charAt(i));
         }
-        if (left1 != right1 || left2 != right2 || left3 != right3) {
-            return false;
-        }
-        //判断左右括号是否匹配
-
-        return true;
+        return stack.empty();
     }
 
 
