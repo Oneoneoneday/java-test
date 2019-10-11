449aec43e8669a4efcccd8233f6a27f1b4a91a82
李永华 <yonghua.li01@hand-china.com>
2019-10-11 20:53:41 +0800

    [FIX] 修改单向有序链表

--- src/leetcode/simple/entity/ListNode.java
+++ src/leetcode/simple/entity/ListNode.java
@@ -10,32 +10,8 @@ public class ListNode {
     public int val;
     public ListNode next;
 
-    public ListNode(){
-
-    }
-
     public ListNode(int x) {
         val = x;
     }
 
-    public ListNode(int x, ListNode next) {
-        val = x;
-        this.next = next;
-    }
-
-    public int getVal() {
-        return val;
-    }
-
-    public void setVal(int val) {
-        this.val = val;
-    }
-
-    public ListNode getNext() {
-        return next;
-    }
-
-    public void setNext(ListNode next) {
-        this.next = next;
-    }
 }
